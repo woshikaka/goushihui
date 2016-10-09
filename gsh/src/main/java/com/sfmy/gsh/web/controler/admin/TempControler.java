aa6668e6add819534de2b9ad4dea609e06c4f593
woshikaka <huangyz1987@sina.com>
2016-10-09 20:19:03 +0800

    Controler层调试成功

---
+++ gsh/src/main/java/com/sfmy/gsh/web/controler/admin/TempControler.java
@@ -0,0 +1,14 @@
+package com.sfmy.gsh.web.controler.admin;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TempControler {
+	@RequestMapping(value = "/test")
+	public String test(HttpServletRequest request) {
+		return "test";
+	}
+}
